382e9d86e0d49eb415e76d00b5f3650d38a95ef1
xrz902 <p.dibrivny@gmail.com>
2026-02-13 21:51:53 +0300

    add: mobile menu

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
-const navbar = document.querySelector('.navbar')
-const logoLight = document.querySelector('.logo-light')
-const logoDark = document.querySelector('.logo-dark')
+const navbar = document.querySelector(".navbar");
+const logoLight = document.querySelector(".logo-light");
+const logoDark = document.querySelector(".logo-dark");
+const mMenuToggle = document.querySelector(".mobile-menu-toggle");
+const menu = document.querySelector(".mobile-menu");
 
-window.addEventListener('scroll', () => {
-  if (this.scrollY > 1) {
-    navbar.classList.add('navbar-light')
-
-    logoLight.classList.add('hidden')
-    logoDark.classList.remove('hidden')
-
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 1) {
+    navbar.classList.add("navbar-light");
+    logoLight.classList.add("hidden");
+    logoDark.classList.remove("hidden");
   } else {
-    navbar.classList.remove('navbar-light')
-
-    logoLight.classList.remove('hidden')
-    logoDark.classList.add('hidden')
+    navbar.classList.remove("navbar-light");
+    logoLight.classList.remove("hidden");
+    logoDark.classList.add("hidden");
   }
-})
+});
+mMenuToggle.addEventListener("click", (event) => {
+  // запускаем ивент
+  event.preventDefault(); // ивент - приказать браузеру не реагировать на это событие
+  menu.classList.toggle("is-open"); // toggle - присвоить
+});
+  
